08636c0b27e960446f03b766ee54881d9318238a
Andre <aramaldes876@gmail.com>
2020-07-11 20:50:28 -0300

    Exercicio da Udemy Busca na internet

---
+++ Busca_Na_Internet/Entite/Link_3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Busca_Na_Internet.Entite
+{
+    class Link_3
+    {
+        public int T { get; set; }
+
+        public Link_3()
+        {
+
+        }
+
+        public Link_3(int t)
+        {
+            T = t;
+        }
+
+        public int PrimeiroLink()
+        {
+            return T * 4;
+        }
+       
+    }
+}
---
+++ Busca_Na_Internet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Busca_Na_Internet.Entite;
+namespace Busca_Na_Internet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            Link_3 _3 = new Link_3(t);
+
+            Console.WriteLine(_3.PrimeiroLink());
+        }
+    }
+}
